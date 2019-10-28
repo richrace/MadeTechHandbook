1c7928a9623e53353e28280f9947952651535b70
George Schena <gs14aaa@herts.ac.uk>
2019-10-28 21:35:11 +0000

    Using props to edit component data

--- components/NavHeader/NavHeader.js
+++ components/NavHeader/NavHeader.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {
-    Text,
-  } from 'react-native';
+import { Text, Image } from 'react-native';
 
-  const NavHeader = () => {
-      return (
-          <Text>Made Tech</Text>
-      )
+import styles from './NavHeaderStyles/NavHeader.style';
+
+
+class NavHeader extends React.Component {
+  render = () => {
+    return (
+      // <Text style={styles.text}>
+      //   {this.props.header}
+        <Image
+          style={styles.image}
+          source={require('../../images/made-tech-logo-colour-fe96d680ccf157972a92723cc5266af6.png')}
+        />
+      // </Text>
+  
+    )
   }
+}
 
-  export { NavHeader };
+export { NavHeader };
---
+++ components/NavHeader/NavHeaderStyles/NavHeader.style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  image: {
+    resizeMode: 'stretch',
+    marginTop: 10,
+    height: 30,    
+    width: 250,
+  }
+});
